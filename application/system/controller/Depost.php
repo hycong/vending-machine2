25ac8356850301f1415f4314504efaa7c0198968
hyc <627005379@qq.com>
2019-05-09 10:44:44 +0800

    修改提现

--- application/system/controller/Depost.php
+++ application/system/controller/Depost.php
@@ -48,15 +48,15 @@ class Depost extends Common
             $remark = input('extract_remark');
             $extract_id = input('extract_id');
             Db::startTrans();
-            $set = ['extract_status' => $status, 'extract_remark' => $remark, 'extract_deal_time' => date('Y-m-d H:i:s'),'extract_deal_agent'=>-1];
+            $set = ['extract_status' => $status, 'extract_result_remark' => $remark, 'extract_deal_time' => date('Y-m-d H:i:s'),'extract_deal_agent'=>-1];
             $result = Db::name('agent_extract')->where(['extract_id' => $extract_id])->setField($set);  // 修改提现信息状态
             if ($result) {   // 修改成功
                 $extract = Db::name('agent_extract')->where(['extract_id' => $extract_id])->find();
                 $agent_map['agent_id'] = ['eq',$extract['extract_agent_id']];
                 $money = $extract['extract_money'] + $extract['extract_tax_money'] + $extract['extract_taxes_dues_money'];
-                if($status == 1 ) {  //  已同意
+                if($status == 2 ) {  //  已同意
                     Db::name('agent')->where($agent_map)->setInc('agent_extract',$money);  // 加上提现成功金额与手续费
-                }else if($status == 2) {   // 已拒绝
+                }else if($status == 3) {   // 已拒绝
                     $agent_info = Db::name('agent')->where($agent_map)->field('agent_balance')->find();
                     $insert_state['statement_agent_id'] = $extract['extract_agent_id'];
                     $insert_state['statement_money'] = $money;
@@ -65,14 +65,14 @@ class Depost extends Common
                     $insert_state['statement_before'] = $agent_info['agent_balance'];      //  拒绝申请前余额 agent_balancer
                     $insert_state['statement_time'] = date('Y-m-d H:i:s',time());
                     Db::name('agent_statement')->insert($insert_state);  // 插入用户提现处理流水信息
-                    Db::name('agent')->where($agent_map)->setInc('agent_balance',$money);  // 返回提现金额
+                    Db::name('agent')->where($agent_map)->inc('agent_balance',$money)->update();  // 返回提现金额
                 }
                 Db::name('agent')->where($agent_map)->setDec('agent_frozen',$money);  // 减去冻结金额
                 Db::commit();
-                return $this->success('处理成功');
+                return returnState(200,'处理成功');
             }else{
                 Db::rollback();
-                return $this->error('审核失败');
+                return returnState(100,'审核失败');
             }
         }
         return $this->fetch('',['info'=>$record_info]);
